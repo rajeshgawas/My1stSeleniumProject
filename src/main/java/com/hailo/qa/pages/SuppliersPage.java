f39ac45216df33329be7d9d5d45df62f8f01ff83
unknown <ubaleashish6@gmail.com>
2021-02-20 02:30:59 +0530

    TC for add,edit,view,delete supplier location is completed

--- src/main/java/com/hailo/qa/pages/SuppliersPage.java
+++ src/main/java/com/hailo/qa/pages/SuppliersPage.java
@@ -12,6 +12,7 @@ import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
@@ -25,6 +26,8 @@ public class SuppliersPage extends TestBase {
 	private static final String supplierEmail = RandomEmailGenerator.generateNewEmailID();
 	private static final String locAdminEmail=RandomEmailGenerator.generateNewEmailID();
 	private static final String locAdminOEmail=RandomEmailGenerator.generateNewEmailID();
+	private static final String locAdminEmail_edit=RandomEmailGenerator.generateNewEmailID();
+	private static final String locAdminOEmail_edit=RandomEmailGenerator.generateNewEmailID();
 	
 	private static Logger logger=Logger.getLogger(SuppliersPage.class.getName());
 	
@@ -149,9 +152,36 @@ public class SuppliersPage extends TestBase {
 
 	@FindBy(xpath = "//span[@class='btn toggle-off btn-sm btn-danger']")
 	WebElement supplierLocStatus_button;
+	
+	@FindBy(xpath = "//span[@class='btn toggle-on btn-sm btn-success']")
+	WebElement supplierLocInactiveStatus_toggle;
 
 	@FindBy(xpath = "//button[contains(text(),'Create')]")
 	WebElement supplierLocCreateAcc_button;
+	
+	@FindBy(xpath = "//button[contains(text(),'Update')]")
+	WebElement editSupplierLocUpdate_button;
+	
+	@FindBy(xpath = "//label[text()='Search:']/Input[@type='search']")
+	WebElement supplierLocSearch_field;
+	
+	@FindBy(xpath = "//tr[1]//a[@title='View']/span")
+	WebElement supplierLocView_icon;
+	
+	@FindBy(xpath = "//div/h4[@class='modal-title']")
+	WebElement supplierLoc_dialogue_header;
+	
+	@FindBy(xpath = "//input[@name='email']")
+	WebElement viewSupplierLoc_dialogue_email;
+	
+	@FindBy(xpath = "//tr[1]//a[@title='Edit']/span")
+	WebElement supplierLocEdit_icon;
+	
+	@FindBy(xpath = "//tr[1]//a[@title='Delete']/span")
+	WebElement supplierLocDelete_icon;
+	
+	@FindBy(xpath = "//button[contains(text(),'Delete')]")
+	WebElement deleteSupplierLoc_Confirm_dialogue_Delete_button;
 
 	public SuppliersPage() {
 		PageFactory.initElements(driver, this);
@@ -222,6 +252,7 @@ public class SuppliersPage extends TestBase {
 
 	}
 	public void verifySupplierIsAdded() throws InterruptedException {
+		logger.info("==============Searching for Supplier===========");
 		supplier_search_field.sendKeys(supplierEmail);
 		Thread.sleep(4000);
 		List<WebElement> supplierList=driver.findElements(By.xpath("//div[@class='card']/div[@class='tabNameList card-body']/div/div[1]"));
@@ -236,17 +267,16 @@ public class SuppliersPage extends TestBase {
 		
 	}
 
-	public void searchSupplier() throws InterruptedException {
+	
+	public void deleteSupplier() throws InterruptedException {
 		suppliers_menu.click();
-		
+		Thread.sleep(2000);
 		verifySupplierIsAdded();
 		Thread.sleep(2000);
-		System.out.println("Supplier email id in searchSupplier method is: " + supplierEmail);
-		String actualSupplierEmail=driver.findElement(By.xpath("//div[@class='card']/div[@class='tabNameList card-body']/div/div[1]//span[2]")).getText().toLowerCase();
-		System.out.println("Actual supplier email id: "+actualSupplierEmail);
-		Assert.assertTrue(actualSupplierEmail.contains(supplierEmail),"Actual supplier email does not match with "+supplierEmail);	
-
+		
+		Thread.sleep(2000);
 	}
+	
 
 	public void addSupplierLocation() throws InterruptedException, AWTException {
 		suppliers_menu.click();
@@ -267,6 +297,7 @@ public class SuppliersPage extends TestBase {
 		List<WebElement> allLocation=driver.findElements(By.xpath("//table[@id='twocolumnlayoutid']/tbody/tr"));
 		int currentLocationCount=allLocation.size();
 		System.out.println("Location count before adding: "+currentLocationCount);
+		logger.info("===================Adding new Supplier Location==================");
 		supplierAddLoc_button.click();
 		Thread.sleep(1000);
 		supplierLoc_textfield.sendKeys("Cincinati");
@@ -274,6 +305,7 @@ public class SuppliersPage extends TestBase {
 		Thread.sleep(1000);
 		supplierLocFirstName_textfield.sendKeys("Kavin");
 		supplierLocLastName_textfield.sendKeys("Peterson");
+		System.out.println("Supplier location email id while adding: "+locAdminEmail);
 		supplierLocEmail_textfield.sendKeys(locAdminEmail);
 		supplierLocOptionEmail_textfield.sendKeys(locAdminOEmail);
 		supplierLocContact_textfield.click();
@@ -296,7 +328,186 @@ public class SuppliersPage extends TestBase {
 		else {
 			Assert.assertTrue(false);
 		}
+		logger.info("===================Supplier Location is added succesfully==================");
+				
+	}
+	public void searchSupplierLocation() throws InterruptedException {
+		suppliers_menu.click();
+		Thread.sleep(2000);
+		verifySupplierIsAdded();
+		Thread.sleep(2000);
+		List<WebElement> supplierList=driver.findElements(By.xpath("//div[@class='card']/div[@class='tabNameList card-body']/div/div[1]"));
+		System.out.println("Number of suppliers: "+supplierList.size());
+		Thread.sleep(2000);
+		if(supplierList.size()==1) {
+			Assert.assertTrue(true);
+		}
+		else {
+			Assert.assertTrue(false);
+		}
+		supplierCard.click();
+		Thread.sleep(2000);
+		System.out.println("Supplier location email id while searching: "+locAdminEmail);
+		supplierLocSearch_field.sendKeys(locAdminEmail);
+		Thread.sleep(2000);
+		List<WebElement> allLocation=driver.findElements(By.xpath("//table[@id='twocolumnlayoutid']/tbody/tr"));
+		int totalLocations=allLocation.size();
+		System.out.println("Total Location count after searching: "+totalLocations);
+		if(totalLocations>0) {
+			Assert.assertTrue(true);
+		}
+		else {
+			Assert.assertTrue(false);
+		}
 		
 	}
+	public void viewSupplierLocation() throws InterruptedException {
+		logger.info("===================Navigating to view Supplier Location window==================");
+		suppliers_menu.click();
+		Thread.sleep(2000);
+		verifySupplierIsAdded();
+		Thread.sleep(2000);
+		List<WebElement> supplierList=driver.findElements(By.xpath("//div[@class='card']/div[@class='tabNameList card-body']/div/div[1]"));
+		System.out.println("Number of suppliers: "+supplierList.size());
+		Thread.sleep(2000);
+		if(supplierList.size()==1) {
+			Assert.assertTrue(true);
+		}
+		else {
+			Assert.assertTrue(false);
+		}
+		supplierCard.click();
+		Thread.sleep(2000);
+		Actions action=new Actions(driver);
+		action.moveToElement(driver.findElement(By.xpath("//table[@id='twocolumnlayoutid']/tbody/tr"))).build().perform();
+		Thread.sleep(2000);
+		supplierLocView_icon.click();
+		Thread.sleep(2000);
+		boolean isHeaderMatchesWithViewSupplierLocationDialogue=supplierLoc_dialogue_header.getText().contains("View");
+		Assert.assertTrue(isHeaderMatchesWithViewSupplierLocationDialogue);
+		String emailFromSupplierLocation=viewSupplierLoc_dialogue_email.getAttribute("value");
+		System.out.println("Current supplier location email id on view page: "+emailFromSupplierLocation);
+		Assert.assertEquals(emailFromSupplierLocation, locAdminEmail,"Current email ");
+		
+	}
+	public void updateSupplierLocationDetails() throws InterruptedException {
+		logger.info("===================Navigating to edit Supplier Location window==================");
+		suppliers_menu.click();
+		Thread.sleep(2000);
+		verifySupplierIsAdded();
+		Thread.sleep(2000);
+		List<WebElement> supplierList=driver.findElements(By.xpath("//div[@class='card']/div[@class='tabNameList card-body']/div/div[1]"));
+		System.out.println("Number of suppliers: "+supplierList.size());
+		Thread.sleep(2000);
+		if(supplierList.size()==1) {
+			Assert.assertTrue(true);
+		}
+		else {
+			Assert.assertTrue(false);
+		}
+		supplierCard.click();
+		Thread.sleep(2000);
+		Actions action=new Actions(driver);
+		action.moveToElement(driver.findElement(By.xpath("//table[@id='twocolumnlayoutid']/tbody/tr"))).build().perform();
+		Thread.sleep(2000);
+		supplierLocEdit_icon.click();
+		Thread.sleep(2000);
+		boolean isHeaderMatchesWithEditSupplierLocationDialogue=supplierLoc_dialogue_header.getText().contains("Edit");
+		Assert.assertTrue(isHeaderMatchesWithEditSupplierLocationDialogue);
+		String editLocationValue=supplierLoc_textfield.getAttribute("value")+"_edit";		
+		String editMinOrderAmtValue="12345.00";
+		String editLocationFirstNameValue=supplierLocFirstName_textfield.getAttribute("value")+"_edit";
+		String editLocationLastNameValue=supplierLocLastName_textfield.getAttribute("value")+"_edit";
+		String editMobilePhoneValue="7676767676";
+		String editAddressLine1Value=supplierLocAddress1_textfield.getAttribute("value")+"_edit";
+		String editAddressLine2Value=supplierLocAddress2_textfield.getAttribute("value")+"_edit";
+		String editCityValue=supplierLocCity_textfield.getAttribute("value")+"_edit";
+		String editStateValue="Alaska";
+		String editZipValue="54321";
+		supplierLoc_textfield.clear();
+		supplierLoc_textfield.sendKeys(editLocationValue);
+		supplierLocMinOrdAmt_textfield.clear();
+		supplierLocMinOrdAmt_textfield.sendKeys(editMinOrderAmtValue);
+		supplierLocFirstName_textfield.clear();
+		supplierLocFirstName_textfield.sendKeys(editLocationFirstNameValue);
+		supplierLocLastName_textfield.clear();
+		supplierLocLastName_textfield.sendKeys(editLocationLastNameValue);
+		supplierLocEmail_textfield.clear();
+		supplierLocEmail_textfield.sendKeys(locAdminEmail_edit);
+		supplierLocContact_textfield.clear();
+		supplierLocContact_textfield.click();
+		supplierLocContact_textfield.sendKeys(editMobilePhoneValue);
+		supplierLocOptionEmail_textfield.clear();
+		supplierLocOptionEmail_textfield.sendKeys(locAdminOEmail_edit);
+		supplierLocCountry_dd.sendKeys("Canada");
+		supplierLocAddress1_textfield.clear();
+		supplierLocAddress1_textfield.sendKeys(editAddressLine1Value);
+		supplierLocAddress2_textfield.clear();
+		supplierLocAddress2_textfield.sendKeys(editAddressLine2Value);
+		supplierLocCity_textfield.clear();
+		supplierLocCity_textfield.sendKeys(editCityValue);
+		supplierLocState_dd.sendKeys(editStateValue);
+		supplierLocZip_textfield.clear();
+		supplierLocZip_textfield.sendKeys(editZipValue);
+		supplierLocInactiveStatus_toggle.click();
+		Thread.sleep(1000);
+		
+		editSupplierLocUpdate_button.click();
+		Thread.sleep(2000);
+		action.moveToElement(driver.findElement(By.xpath("//table[@id='twocolumnlayoutid']/tbody/tr"))).build().perform();
+		Thread.sleep(2000);
+		logger.info("===================Navigating to view Supplier Location window==================");
+		supplierLocView_icon.click();		
+		Thread.sleep(2000);
+		boolean isHeaderMatchesWithViewSupplierLocationDialogue=supplierLoc_dialogue_header.getText().contains("View");
+		Assert.assertTrue(isHeaderMatchesWithViewSupplierLocationDialogue);
+		String emailFromSupplierLocation=viewSupplierLoc_dialogue_email.getAttribute("value");
+		System.out.println("Current supplier location email id on view page: "+emailFromSupplierLocation);
+		System.out.println("Current Location name: "+supplierLoc_textfield.getAttribute("value"));
+		System.out.println("Current minimum order amount: "+supplierLocMinOrdAmt_textfield.getAttribute("value"));
+		
+		System.out.println("Current First name of location: "+supplierLocFirstName_textfield.getAttribute("value"));
+		Assert.assertEquals(supplierLoc_textfield.getAttribute("value"), editLocationValue,"Current location name does not match with changed location name");
+		Assert.assertEquals(supplierLocMinOrdAmt_textfield.getAttribute("value"), editMinOrderAmtValue,"Current Minimum order amount with changed order amount");
+		Assert.assertEquals(supplierLocFirstName_textfield.getAttribute("value"), editLocationFirstNameValue,"Current first name does not match with changed first name");
+		Assert.assertEquals(supplierLocLastName_textfield.getAttribute("value"), editLocationLastNameValue,"Current last name does not match with changed last name");
+		Assert.assertEquals(supplierLocEmail_textfield.getAttribute("value"), locAdminEmail_edit,"Current email does not match with changed email");		
+		logger.info("===================Supplier Location updated successfully==================");
+	}
+	public void deleteSupplierLocation() throws InterruptedException {
+		suppliers_menu.click();
+		Thread.sleep(2000);
+		verifySupplierIsAdded();
+		Thread.sleep(2000);
+		List<WebElement> supplierList=driver.findElements(By.xpath("//div[@class='card']/div[@class='tabNameList card-body']/div/div[1]"));
+		System.out.println("Number of suppliers: "+supplierList.size());
+		Thread.sleep(2000);
+		if(supplierList.size()==1) {
+			Assert.assertTrue(true);
+		}
+		else {
+			Assert.assertTrue(false);
+		}
+		supplierCard.click();
+		Thread.sleep(2000);
+		Actions action=new Actions(driver);
+		action.moveToElement(driver.findElement(By.xpath("//table[@id='twocolumnlayoutid']/tbody/tr"))).build().perform();
+		Thread.sleep(2000);
+		logger.info("===================Navigating to delete Supplier Location window==================");
+		supplierLocDelete_icon.click();
+		boolean isHeaderMatchesWithDeleteSupplierLocationDialogue=supplierLoc_dialogue_header.getText().contains("Delete");
+		Assert.assertTrue(isHeaderMatchesWithDeleteSupplierLocationDialogue);
+		deleteSupplierLoc_Confirm_dialogue_Delete_button.click();
+		Thread.sleep(2000);
+		String tableTextWhenZeroLocationIsPresent=driver.findElement(By.xpath("//table[@id='twocolumnlayoutid']/tbody/tr/td")).getText();
+		System.out.println("Table text when zero supplier location: "+tableTextWhenZeroLocationIsPresent);
+		if(tableTextWhenZeroLocationIsPresent.equalsIgnoreCase("No data available in table")) {
+			Assert.assertTrue(true);
+		}
+		else {
+			Assert.assertTrue(false);
+		}
+		logger.info("===================Supplier Location deleted successfully==================");
+	}	
 
 }
--- src/test/java/com/hailo/qa/testcases/AddSupplierTest.java
+++ src/test/java/com/hailo/qa/testcases/AddSupplierTest.java
@@ -1,5 +1,8 @@
 package com.hailo.qa.testcases;
 
+import java.awt.AWTException;
+
+import org.apache.log4j.Logger;
 import org.testng.Assert;
 //import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
@@ -20,40 +23,89 @@ public class AddSupplierTest extends TestBase {
 	LoginPage loginPage;
 	Dashboard dashboard;
 	SuppliersPage suppliersPage;
+	private static Logger logger=Logger.getLogger(CustomersTest.class.getName());
 
 	@BeforeMethod
 	public void setUp() throws InterruptedException {
 		initialization();
 		loginPage = new LoginPage();
-		loginPage.login(prop.getProperty("email"), prop.getProperty("password"));
+		loginPage.loginWithAdmin(prop.getProperty("admin_email"), prop.getProperty("admin_password"));
 		suppliersPage = new SuppliersPage();
-		// dashboard = new Dashboard();
-
 	}
 
 	@Test(priority = 1)
-	public void createNewSupplier() throws InterruptedException {
+	public void createNewSupplierTest() throws InterruptedException {
 		dashboard = new Dashboard();
 		dashboard.clickonAdminMenu();
+		logger.info("*******************Add new supplier test executon started******************");
 		suppliersPage.openAddSupplierWindow();
 		Thread.sleep(2000);
+		logger.info("==============Entering Supplier details===========");
 		suppliersPage.enterSupplierName();
-		suppliersPage.enterSupplierFirstName();
+		suppliersPage.enterSupplierFirstName()	;
 		suppliersPage.enterSupplierLastName();
 		suppliersPage.enterEmail();
 		suppliersPage.enterSupplierContact();
 		Thread.sleep(2000);
 		suppliersPage.enableStatus();
-		Thread.sleep(500);
+		Thread.sleep(2000);
 		suppliersPage.enableCheckboxes();
-		suppliersPage.uploadDoc();
 		Thread.sleep(5000);
 		suppliersPage.clickCreateAccountButton();
 		Thread.sleep(2000);
+		driver.navigate().refresh();
+		Thread.sleep(3000);
+		suppliersPage.verifySupplierIsAdded();
+		logger.info("==============Supplier is added successfully===========");
+		logger.info("*******************Add new supplier test executon completed******************");
 	}
+		
+//		@Test(priority = 2)
+//		public void searchSupplier () throws InterruptedException{
+//			logger.info("*******************Search supplier test executon started******************");
+//			dashboard = new Dashboard();
+//			dashboard.clickonAdminMenu();
+//			suppliersPage.searchSupplier();
+//			Thread.sleep(1000);	
+//			logger.info("*******************Search supplier test executon completed******************");
+//		}
+//		@Test(priority = 3)
+//		public void viewSupplierLocationTest () throws InterruptedException{
+//			logger.info("*******************View supplier location test executon started******************");
+//			dashboard = new Dashboard();
+//			dashboard.clickonAdminMenu();
+//			suppliersPage.viewSupplierLocation();
+//			Thread.sleep(1000);	
+//			logger.info("*******************View supplier test executon completed******************");
+//		}
+		
+		@Test(priority=2)
+		public void addNewSupplierLocationTest() throws InterruptedException, AWTException {
+			logger.info("*******************Add supplier location test executon started******************");
+			dashboard = new Dashboard();
+			dashboard.clickonAdminMenu();
+			suppliersPage.addSupplierLocation();
+			logger.info("*******************Add supplier location test executon completed******************");
+		}
+		@Test(priority=3)
+		public void editNewSupplierLocationTest() throws InterruptedException, AWTException {
+			logger.info("*******************Edit supplier location test executon started******************");
+			dashboard = new Dashboard();
+			dashboard.clickonAdminMenu();
+			suppliersPage.updateSupplierLocationDetails();;
+			logger.info("*******************Edit supplier location test executon completed******************");
+		}
+		@Test(priority=4)
+		public void deleteSupplierLocationTest() throws InterruptedException, AWTException {
+			logger.info("*******************Delete supplier location test executon started******************");
+			dashboard = new Dashboard();
+			dashboard.clickonAdminMenu();
+			suppliersPage.deleteSupplierLocation();
+			logger.info("*******************Delete supplier location test executon completed******************");
+		}	
 
 	@AfterMethod
 	public void tearDown() {
-		driver.quit();
+		//driver.quit();
 	}
 }
